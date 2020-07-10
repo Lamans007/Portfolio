47129ac91f4828d2ea3f4d2e32dfa24fd7b5c3d5
Lamans007 <55312377+Lamans007@users.noreply.github.com>
2020-07-10 23:16:22 +0100

    Updates

---
+++ precache-manifest.0aa11e460cccb4b192e189c1a57ee4af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb843d763aa861de9279d066401508b",
+    "url": "/Portfolio/react-project/index.html"
+  },
+  {
+    "revision": "54b9d46019de0c70fefa",
+    "url": "/Portfolio/react-project/static/css/main.67e4a0a4.chunk.css"
+  },
+  {
+    "revision": "104730a8bd1eda32b599",
+    "url": "/Portfolio/react-project/static/js/2.7ac2f945.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/react-project/static/js/2.7ac2f945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b9d46019de0c70fefa",
+    "url": "/Portfolio/react-project/static/js/main.db3fe4ee.chunk.js"
+  },
+  {
+    "revision": "2579fe2b6c7a867432bb",
+    "url": "/Portfolio/react-project/static/js/runtime-main.6c9387cd.js"
+  }
+]);
